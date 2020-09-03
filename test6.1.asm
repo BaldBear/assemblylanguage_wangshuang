8fcb70c01280803d0ce4ef526ba0ae4cc5801bd1
Erving <erving93@163.com>
2020-09-03 23:12:14 +0800

    20200903

---
+++ test6.1.asm
@@ -0,0 +1,22 @@
+assume cs:code
+
+code segment
+
+	dw 0123h,0456h,0789h,0abch,0defh,0fedh,0cbah,0987h
+
+start:	mov ax, 0
+		mov ds, ax
+		mov bx, 0
+		mov cx, 8
+
+	s:	mov ax, ds:[bx]
+		mov cs:[bx], ax
+		add bx, 2
+		loop s
+
+		mov ax,4C00H
+		int 21H
+
+code ends
+
+end start
